29f8e8ace0dd52265e5d814cc1a232720bc86742
Gabo Lugo <gabovanlugo@gmail.com>
2019-02-16 00:50:00 -0600

    Connect to MongoDB and setup up routes.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ const fastify = require('fastify')({
   logger: true,
 });
 
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+// Connect to DB
+mongoose.connect('mongodb://localhost/mystore')
+  .then(() => console.log('MongoDB connected...'))
+  .then(err => console.log(err));
+
 fastify.get('/', async () => ({ hello: 'world' }));
+
+routes.forEach((route) => {
+  fastify.route(route);
+});
 
 const start = async () => {
   try {
