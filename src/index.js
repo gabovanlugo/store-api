acbb9a02450e34c9757ac106920a9c9de326c03a
Gabo Lugo <gabovanlugo@gmail.com>
2019-02-16 01:00:42 -0600

    Integrate Swagger.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ const fastify = require('fastify')({
 
 const mongoose = require('mongoose');
 
+// Swagger options
+const swagger = require('./config/swagger');
+fastify.register(require('fastify-swagger'), swagger.options);
+
 const routes = require('./routes');
 
 // Connect to DB
 mongoose.connect('mongodb://localhost/mystore')
   .then(() => console.log('MongoDB connected...'))
-  .then(err => console.log(err));
-
-fastify.get('/', async () => ({ hello: 'world' }));
+  .catch(err => console.log(err));
 
 routes.forEach((route) => {
   fastify.route(route);
@@ -20,6 +22,7 @@ routes.forEach((route) => {
 const start = async () => {
   try {
     await fastify.listen(3000);
+    fastify.swagger();
     fastify.log.info(`Server on: ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
